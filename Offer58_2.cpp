e4aa2fef367fdd2cee2e938e0d1ccedab667639b
Gh-story <hg820801350@icloud.com>
2021-05-02 13:48:29 +0800

    Offer58

--- Offer58_2.cpp
+++ Offer58_2.cpp
@@ -1,2 +1,11 @@
 #include<iostream>
 using namespace std;
+class Solution {
+public:
+    //method one:4ms-8ms
+    string reverseLeftWords(string s, int n) {
+        string s1 = s.substr(0,n);
+        string s2 = s.substr(n,s.size());
+        return s2+s1;
+    }
+};
