9498d3afc378fadff0784247177ec208a8fea46e
Gh-story <hg820801350@icloud.com>
2021-05-04 14:07:59 +0800

    Offer65

---
+++ Offer62.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int f(int n, int m) {
+        if (n == 1) {
+            return 0;
+        }
+        int x = f(n - 1, m);
+        return (m + x) % n;
+    }
+public:
+    int lastRemaining(int n, int m) {
+        return f(n, m);
+    }
+
+
+};
---
+++ Offer65.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int add(int a, int b) {
+        while(b!=0){
+            int c  = (unsigned int)(a&b)<<1;
+            a = a^b;
+            b = c;
+        }
+        return a;
+    }
+};
