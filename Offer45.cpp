69c125db73a15e1a051951930b5ad2ed2a3cc010
Gh-story <hg820801350@icloud.com>
2021-06-30 12:31:36 +0800

    Offer46

--- Offer45.cpp
+++ Offer45.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<algorithm>
 using namespace std;
 class Solution {
 public:
     string minNumber(vector<int>& nums) {
-
+        vector<string> strs;
+        for(int i =0;i<nums.size();i++){
+            strs.push_back(to_string(nums[i]));
+        }
+        quickSort(strs,0, nums.size()-1);
+        string ans;
+        for(string s : strs)
+            ans.append(s);
+        return ans;
+    }
+private:
+    void quickSort(vector<string>&strs,int l, int r){
+        if(l >= r) return;
+        int i = l, j = r;
+        while(i < j) {
+            while(strs[j] + strs[l] >= strs[l] + strs[j] && i < j) j--;
+            while(strs[i] + strs[l] <= strs[l] + strs[i] && i < j) i++;
+            swap(strs[i], strs[j]);
+        }
+        swap(strs[i], strs[l]);
+        quickSort(strs, l, i - 1);
+        quickSort(strs, i + 1, r);
     }
 };
 /*
