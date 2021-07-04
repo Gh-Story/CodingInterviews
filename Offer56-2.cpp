ea2b4fb828a40db6e543ad9a4a1e4d59325f4c18
Gh-story <hg820801350@icloud.com>
2021-07-04 22:50:21 +0800

    Offer56-2

--- Offer56-2.cpp
+++ Offer56-2.cpp
@@ -3,3 +3,22 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sum[32];
+        memset(sum, 0, sizeof(sum));
+        for(int num:nums){
+            for(int j=0;j<32;j++){
+                sum[j] +=num&1;
+                num>>=1;
+            }
+        }
+        int ans =0,mods=3;
+        for(int i =0;i<32;i++){
+            ans<<=1;
+            ans|=sum[31-i]%mods;
+        }
+        return ans;
+    }
+};
